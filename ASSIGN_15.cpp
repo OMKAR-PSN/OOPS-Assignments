3903e8f8faeb3cf55d804603c159472ecf1013c4
OMKAR-PSN <omkar.nikam3016@gmail.com>
2025-04-23 23:00:17 -0700

    Update ASSIGN_15.cpp

--- ASSIGN_15.cpp
+++ ASSIGN_15.cpp
@@ -1,1 +1,60 @@
+/*Create a class named Shape with a function that prints "This is a shape". Create another class 
+named Polygon inheriting the Shape class with the same function that prints "Polygon is a shape". 
+Create two other classes named Rectangle and Triangle having the same function which prints 
+"Rectangle is a polygon" and "Triangle is a polygon" respectively. Again, make another class named 
+Square having the same function which prints "Square is a rectangle". 
+Now, try calling the function by the object of each of these classes.*/
+_______________________________________________________________________________________________________________
+#include <iostream> 
+using namespace std; 
+class Shape { 
+public: 
+void display() { 
+cout << "This is a shape" << endl; 
+    } 
+}; 
+ 
+class Polygon : public Shape { 
+public: 
+    void display() { 
+        cout << "Polygon is a shape" << endl; 
+    } 
+}; 
+ 
+class Rectangle : public Polygon { 
+public: 
+    void display() { 
+        cout << "Rectangle is a polygon" << endl; 
+    } 
+}; 
+ 
+class Triangle : public Polygon { 
+public: 
+    void display() { 
+        cout << "Triangle is a polygon" << endl; 
+    } 
+}; 
+ 
+class Square : public Rectangle { 
+public: 
+    void display() { 
+        cout << "Square is a rectangle" << endl; 
+    } 
+}; 
+ 
+int main() { 
+  Shape s; 
+  Polygon p; 
+  Rectangle r; 
+  Triangle t; 
+  Square sq; 
+  
+  cout << "\nCalling display() for each object:\n"; 
+  s.display(); 
+  p.display(); 
+  r.display(); 
+  t.display(); 
+  sq.display(); 
 
+  return 0; 
+}
